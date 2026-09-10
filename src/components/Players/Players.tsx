679c8c2e05db2d0f954632c21c7b86b6240909d5
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-10 11:29:59 +0600

    Modified-Btn:Toggleing

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -1,6 +1,7 @@
 import React, { use, useState } from 'react';
 import type { PlayersType } from '../../types';
 import AvailablePlayers from './AvailablePlayers';
+import SelectedPlayers from './SelectedPlayers';
 
 interface PlayersProps {
     playersPromise: Promise<PlayersType[]>;
@@ -19,7 +20,7 @@ const Players = ({ playersPromise }: PlayersProps) => {
         <div className='w-11/12 mx-auto'>
 
             <div className='flex justify-between gap-4 mb-2'>
-                <h2 className='font-bold text-xl'>Available Players</h2>
+                <h2 className='font-bold text-xl'>{buttonType === "available" ? "Available Players" : "Selected Players"}</h2>
 
                 <div>
                     <button onClick={() => handleBtnTypeUpdate("available")} className={`btn btn-active ${buttonType === "available" ? "btn-success" : ""} rounded-r-none`}>Available</button>
@@ -28,7 +29,11 @@ const Players = ({ playersPromise }: PlayersProps) => {
                 </div>
             </div>
             
-            <AvailablePlayers players={players} />
+            {buttonType === "available" ? (
+                <AvailablePlayers players={players} />
+            ) : (
+                <SelectedPlayers />
+            )}
         
         </div>
     )
